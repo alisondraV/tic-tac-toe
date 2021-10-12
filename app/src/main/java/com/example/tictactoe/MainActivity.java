813e43548cec54179f2532d7472a14d1596ab0d0
alisondraV <alice.7414122013@gmail.com>
2021-10-12 17:47:12 -0400

    Add score keeping and display

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int NUMBER_OF_CLICKS_TO_DRAW = 9;
     Button newGameButton;
+    TextView playerOneTextView;
+    TextView playerTwoTextView;
     Button[][] ticTacToeButtons = new Button[3][3];
 
+    private int cellsClickedInCurrentGame = 0;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
     private boolean xTurn = true;
     String[] winningCombinations = {
             "000102",
@@ -25,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             "011121",
             "021222",
             "001122",
-            "021120",
+            "021120"
     };
 
     @Override
@@ -34,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         newGameButton = findViewById(R.id.btnNewGame);
+        playerOneTextView = findViewById(R.id.txtPlayerOne);
+        playerTwoTextView = findViewById(R.id.txtPlayerTwo);
         ticTacToeButtons[0][0] = findViewById(R.id.btn00);
         ticTacToeButtons[0][1] = findViewById(R.id.btn01);
         ticTacToeButtons[0][2] = findViewById(R.id.btn02);
@@ -50,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ticTacToeButtons[i][j].setOnClickListener(this);
             }
         }
+
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
     }
 
     @Override
@@ -83,9 +96,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     xTurn = !xTurn;
                 }
 
-                checkGame();
             }
         }
+        checkGame();
     }
 
     private void newGame() {
@@ -99,14 +112,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void checkGame() {
         for (String c: winningCombinations) {
-            String firstValue = (String) ticTacToeButtons[toInt(c.charAt(0))][toInt(c.charAt(1))].getText();
-            String secondValue = (String) ticTacToeButtons[toInt(c.charAt(2))][toInt(c.charAt(3))].getText();
-            String thirdValue = (String) ticTacToeButtons[toInt(c.charAt(4))][toInt(c.charAt(5))].getText();
+            String firstValue = (String) ticTacToeButtons[charToInt(c.charAt(0))][charToInt(c.charAt(1))].getText();
+            String secondValue = (String) ticTacToeButtons[charToInt(c.charAt(2))][charToInt(c.charAt(3))].getText();
+            String thirdValue = (String) ticTacToeButtons[charToInt(c.charAt(4))][charToInt(c.charAt(5))].getText();
 
             if (firstValue.equals(secondValue) && secondValue.equals(thirdValue) && !thirdValue.equals("")) {
-                Toast.makeText(this, firstValue + " won", Toast.LENGTH_SHORT).show();
+                if (firstValue.equals("X")) {
+                    playerOneScore++;
+                    playerOneTextView.setText(playerOneScore + "");
+                    Toast.makeText(this, "Player One has won!", Toast.LENGTH_SHORT).show();
+                } else {
+                    playerTwoScore++;
+                    playerTwoTextView.setText(playerTwoScore + "");
+                    Toast.makeText(this, "Player Two has won!", Toast.LENGTH_SHORT).show();
+                }
                 stopGame();
             }
+        }
+
+        if (++cellsClickedInCurrentGame == NUMBER_OF_CLICKS_TO_DRAW) {
+            Toast.makeText(this, "Game Draw!!", Toast.LENGTH_SHORT).show();
+            stopGame();
         }
     }
 
@@ -116,13 +142,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ticTacToeButtons[i][j].setEnabled(false);
             }
         }
+        xTurn = true;
+        cellsClickedInCurrentGame = 0;
     }
 
     private boolean isButtonEligibleToBeMarked(View view, Button button) {
         return view.getId() == button.getId() && button.getText() == "";
     }
 
-    private int toInt(char c) {
+    private int charToInt(char c) {
         return Character.getNumericValue(c);
     }
 }
