42bda75676e199be3d7cbfefbb39d5756f2ea787
alisondraV <alice.7414122013@gmail.com>
2021-11-19 12:01:18 -0500

    Create main menu activity and navigation to the game

---
+++ app/src/main/java/com/example/tictactoe/MainMenuActivity.java
@@ -0,0 +1,44 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainMenuActivity extends AppCompatActivity implements OnClickListener {
+    Button startGame;
+    Button scoreboard;
+    Button selectPlayer1;
+    Button selectPlayer2;
+    Button addPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        startGame = findViewById(R.id.btn_start);
+        scoreboard = findViewById(R.id.btn_scoreboard);
+        selectPlayer1 = findViewById(R.id.btn_player1);
+        selectPlayer2 = findViewById(R.id.btn_player2);
+        addPlayer = findViewById(R.id.btn_add_player);
+
+        startGame.setOnClickListener(this);
+        scoreboard.setOnClickListener(this);
+        selectPlayer1.setOnClickListener(this);
+        selectPlayer2.setOnClickListener(this);
+        addPlayer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start:
+                Intent gameActivity = new Intent(this, MainActivity.class);
+                this.startActivity(gameActivity);
+        }
+    }
+}
