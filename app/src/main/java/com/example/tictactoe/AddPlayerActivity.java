f9441a36ed73a17fef86d1d3b07656eff082b766
alisondraV <alice.7414122013@gmail.com>
2021-11-23 16:05:44 -0500

    Add AddPlayerActivity

---
+++ app/src/main/java/com/example/tictactoe/AddPlayerActivity.java
@@ -0,0 +1,26 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddPlayerActivity extends AppCompatActivity {
+    EditText playerName;
+    Button addPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player);
+
+        playerName = findViewById(R.id.playerName);
+        addPlayer = findViewById(R.id.btnAddPlayerName);
+
+        addPlayer.setOnClickListener(view -> {
+            String name = String.valueOf(playerName.getText());
+        });
+    }
+}
