20547a86400d1118bdf5e614d0cbc9eda130513a
alisondraV <alice.7414122013@gmail.com>
2021-11-27 11:47:51 -0500

    Update AddPlayerActivity to connect with the db

--- app/src/main/java/com/example/tictactoe/AddPlayerActivity.java
+++ app/src/main/java/com/example/tictactoe/AddPlayerActivity.java
@@ -1,14 +1,26 @@
 package com.example.tictactoe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddPlayerActivity extends AppCompatActivity {
     EditText playerName;
     Button addPlayer;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Player player;
+    long maxId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +30,41 @@ public class AddPlayerActivity extends AppCompatActivity {
         playerName = findViewById(R.id.etPlayerName);
         addPlayer = findViewById(R.id.btnAddPlayerName);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("Player");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                maxId = snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
+
         addPlayer.setOnClickListener(view -> {
             String name = String.valueOf(playerName.getText());
+
+            if (name.equals("")) {
+                Toast.makeText(AddPlayerActivity.this, "You should specify a player name", Toast.LENGTH_SHORT).show();
+            } else {
+                player = new Player(maxId + 1, name, 0, 0, 0);
+                databaseReference.child(String.valueOf(player.getId())).setValue(player);
+            }
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Toast.makeText(AddPlayerActivity.this, "Player has been successfully created!", Toast.LENGTH_SHORT).show();
+                    playerName.setText("");
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(AddPlayerActivity.this, "An error occurred while creating a player :(", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
---
+++ app/src/main/java/com/example/tictactoe/Player.java
@@ -0,0 +1,62 @@
+package com.example.tictactoe;
+
+public class Player {
+    private long id;
+    private String name;
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public Player(long id, String name, int wins, int losses, int ties) {
+        this.id = id;
+        this.name = name;
+        this.wins = wins;
+        this.losses = losses;
+        this.ties = ties;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getTies() {
+        return ties;
+    }
+
+    public void setTies(int ties) {
+        this.ties = ties;
+    }
+
+    @Override
+    public String toString() {
+        return "Player " + id + ": " + name;
+    }
+}
